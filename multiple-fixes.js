fabbae5e2419a872c0261e09f18575cd5f24a152
Sam Xu <xcjsam@arista.com>
2025-09-09 18:38:31 -0700

    Add multiple bug fixes

---
+++ multiple-fixes.js
@@ -0,0 +1,8 @@
+// Test file for multiple valid trailers
+console.log("Testing multiple valid MRFT trailers");
+
+function multipleFixes() {
+    return "This demonstrates multiple valid trailers on separate lines";
+}
+
+module.exports = { multipleFixes };
