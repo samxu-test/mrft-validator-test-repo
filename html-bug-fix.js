2b2dcdf7042755f816eb62c07f5c0c20ede7dbf1
Sam Xu <xcjsam@arista.com>
2025-09-09 18:28:32 -0700

    Fix HTML tag issue

---
+++ html-bug-fix.js
@@ -0,0 +1,8 @@
+// Test file with HTML tag issues in MRFT trailers
+console.log("Testing HTML tags in trailers");
+
+function htmlTagFix() {
+    return "This demonstrates HTML tag validation";
+}
+
+module.exports = { htmlTagFix };// Test HTML tags validation
