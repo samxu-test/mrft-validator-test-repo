cd2f50b07dcfeddf27f7238d4d76d0661cc9f261
Sam Xu <xcjsam@arista.com>
2025-09-09 17:55:04 -0700

    Add test file for MRFT validation

---
+++ test-file.js
@@ -0,0 +1,8 @@
+// Simple test file for MRFT validation
+console.log("Testing MRFT trailer validation");
+
+function testFunction() {
+    return "This is a test function";
+}
+
+module.exports = { testFunction };
